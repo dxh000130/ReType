ce018f1d1752afa373406bd7a42f6ba2118c32f2
Duan Xuheng <dxh000130@vip.126.com>
2022-04-06 00:12:01 +1200

    Fix bug for Highlight, Add Score, Valid correct done
    Co-Authored-By: Alex Qin <100546116+Pqin95@users.noreply.github.com>

--- Endpoint/ReType/Controllers/GameController.cs
+++ Endpoint/ReType/Controllers/GameController.cs
@@ -56,17 +56,32 @@ namespace ReType.Controllers
             string username = claim.Value;
             string pattern1 = @"[A-Za-z]*(?i:";
             string pattern3 = ")+[A-Za-z]*";
-            string pattern = pattern1 + Article.Input + pattern3;
-            string replacement = "<span style=\"color: blue;\">$&</span>";
-            Regex rgx = new Regex(pattern);
-            //string result = rgx.Replace(Article.Article, replacement);
+            string inputreplace = "";
+            Match mc2 = Regex.Match(Article.Input, "[A-Za-z]+");
+            if (mc2.Value != "")
+            {
+                inputreplace = mc2.Value;
+            }
+            else
+            {
+                inputreplace = "sfgvbadfvbhaidfgvbadfvagarfgsdfgvfdvdfvfvdfsbvb";
+            }
+
+            Regex inputrgx = new Regex(".+");
+            Article.Input = inputrgx.Replace(Article.Input, inputreplace);
+
             string[] wronglist = _repository.WrongWordList(Article.ArticleID);
             string[] correctlist = _repository.CorrectWordList(Article.ArticleID);
+            string pattern = pattern1 + Article.Input + pattern3;
+            string replacement = "<span style=\"color: LightGray;\">$&</span>";
+            Regex rgx = new Regex(pattern);
+            //string result = rgx.Replace(Article.Article, replacement);
+            
             string already = Article.AlreadyCorrect;
             string articlecopy = Article.Article;
             for (int i = 0; i < wronglist.Length; i++)
             {
-                if (Regex.Matches(already, Article.Input+ ",").Count >= 1)
+                if (wronglist[i].ToUpper() == Article.Input.ToUpper() && Article.Enter == 1)
                 {
                     int error_remain = wronglist.Count();
                     if (already != "")
@@ -75,15 +90,28 @@ namespace ReType.Controllers
                     }
 
                     string result = rgx.Replace(Article.Article, replacement);
-                    Article_Process_out final = new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "Already Input", ArticleDisp = result, ErrorRemain = error_remain, AlreadyCorrect = already, Score = _repository.GetUserScore(username) };
+                    Article_Process_out final = new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "tryagain, No plus or minus score", ArticleDisp = result, ErrorRemain = error_remain, AlreadyCorrect = already, Score = _repository.GetUserScore(username) };
                     return Ok(final);
                 }
-                else if (wronglist[i] == Article.Input)
+                    if (Regex.Matches(already, "(?i:" + Article.Input+ "),").Count >= 1 && Article.Enter == 1)
                 {
+                    int error_remain = wronglist.Count();
+                    if (already != "")
+                    {
+                        error_remain = wronglist.Count() - already.Split(',').Count() / 2;
+                    }
+
+                    string result = rgx.Replace(Article.Article, replacement);
+                    Article_Process_out final = new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "Already Input, No plus or minus score", ArticleDisp = result, ErrorRemain = error_remain, AlreadyCorrect = already, Score = _repository.GetUserScore(username) };
+                    return Ok(final);
+                }
+                else if (correctlist[i].ToUpper() == Article.Input.ToUpper() && Article.Enter == 1)
+                {
+                    Regex rgx12 = new Regex(wronglist[i]);
                     string correctreplacement = "<span style=\"color: green;\">" + correctlist[i] + "</span>";
-                    string result = rgx.Replace(Article.Article, correctreplacement);
+                    string result = rgx12.Replace(Article.Article, correctreplacement);
                     string correctreplacementnocolor = correctlist[i];
-                    string articleout = rgx.Replace(articlecopy, correctreplacementnocolor);
+                    string articleout = rgx12.Replace(articlecopy, correctreplacementnocolor);
                     if (already == "")
                     {
                         already += wronglist[i] + "," + correctlist[i];
@@ -93,7 +121,7 @@ namespace ReType.Controllers
                         already += "," + wronglist[i] + "," + correctlist[i];
                     }
                     int error_remain = wronglist.Count() - already.Split(',').Count() / 2;
-                    Article_Process_out final = new Article_Process_out { ArticleID = Article.ArticleID, Article = articleout, Correct = "yes", ArticleDisp = result, ErrorRemain = error_remain, AlreadyCorrect = already, Score = _repository.AddUserScore(username) };
+                    Article_Process_out final = new Article_Process_out { ArticleID = Article.ArticleID, Article = articleout, Correct = "yes,add 1 score", ArticleDisp = result, ErrorRemain = error_remain, AlreadyCorrect = already, Score = _repository.AddUserScore(username) };
                     return Ok(final);
                 }
                 
@@ -103,8 +131,17 @@ namespace ReType.Controllers
             {
                 error_remain1 = wronglist.Count() - already.Split(',').Count() / 2;
             }
-            string result1 = rgx.Replace(Article.Article, replacement);
-            Article_Process_out final1 = new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "No", ArticleDisp = result1, ErrorRemain = error_remain1, AlreadyCorrect = already, Score = _repository.GetUserScore(username) };
+
+            //string result1 = rgx.Replace(Article.Article, replacement);
+            Regex rgx2 = new Regex("(?i:" + Article.Input + ")+");
+            string result2 = rgx2.Replace(Article.Article, "<span style=\"color: blue;\">$&</span>");
+            Regex rgx3 = new Regex("[A-Za-z]*<span style=\"color: blue;\">(?i:" + Article.Input + ")+</span>[A-Za-z]*");
+            string result3 = rgx3.Replace(result2, "<span style=\"background-color: DarkGray;\">$&</span>");
+            Article_Process_out final1 = new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "No, No plus or minus score", ArticleDisp = result3, ErrorRemain = error_remain1, AlreadyCorrect = already, Score = _repository.GetUserScore(username) };
+            if (articlecopy == result3 && Article.Enter==1)
+            {
+                return Ok(new Article_Process_out { ArticleID = Article.ArticleID, Article = articlecopy, Correct = "No, minus score", ArticleDisp = result3, ErrorRemain = error_remain1, AlreadyCorrect = already, Score = _repository.MinusUserScore(username) });
+            }
             return Ok(final1);
         }
     }
