6f22cf1efdfcee7d56ecad46c239c56465c4a7f8
Shelvin Liu <375577756@qq.com>
2022-04-09 21:17:40 +1200

    changed some content to English

--- Web/game/read.js
+++ Web/game/read.js
@@ -1,95 +1,104 @@
 //阅读控件(字体大小，字体粗细，字体family，背景颜色，全屏缩屏)
 //字体大小
-if(!localStorage.zitidaxiao)
-{localStorage.zitidaxiao="20";
-document.getElementById("content").style.fontSize=localStorage.zitidaxiao+'px';}
-else
-document.getElementById("content").style.fontSize=localStorage.zitidaxiao+'px';    
-function zitibianda(){
-localStorage.zitidaxiao++;
-document.getElementById("content").style.fontSize=localStorage.zitidaxiao+'px';
+if (!localStorage.zitidaxiao) {
+    localStorage.zitidaxiao = "20";
+    document.getElementById("content").style.fontSize = localStorage.zitidaxiao + 'px';
+} else
+    document.getElementById("content").style.fontSize = localStorage.zitidaxiao + 'px';
+
+function zitibianda() {
+    localStorage.zitidaxiao++;
+    document.getElementById("content").style.fontSize = localStorage.zitidaxiao + 'px';
 }
-function zitibianxiao(){
-localStorage.zitidaxiao--;
-document.getElementById("content").style.fontSize=localStorage.zitidaxiao+'px';
+
+function zitibianxiao() {
+    localStorage.zitidaxiao--;
+    document.getElementById("content").style.fontSize = localStorage.zitidaxiao + 'px';
 }
 
 
 //字体粗细
-if(localStorage.jiacu)
-document.getElementById("content").style.fontWeight=localStorage.jiacu;
-function jiacu(){
-if(document.getElementById("content").style.fontWeight!="611")
-{document.getElementById("content").style.fontWeight="611";
-localStorage.jiacu="611";}
-else
-{document.getElementById("content").style.fontWeight="normal";
-localStorage.jiacu="normal";}
+if (localStorage.jiacu)
+    document.getElementById("content").style.fontWeight = localStorage.jiacu;
+
+function jiacu() {
+    if (document.getElementById("content").style.fontWeight != "611") {
+        document.getElementById("content").style.fontWeight = "611";
+        localStorage.jiacu = "611";
+    } else {
+        document.getElementById("content").style.fontWeight = "normal";
+        localStorage.jiacu = "normal";
+    }
 }
 
 //字体family，字体family按键自适应
-if(!localStorage.ziti)
-{localStorage.ziti="SimSun";
-localStorage.zitiname="宋体";
-document.getElementById("content").style.fontFamily=localStorage.ziti;
-document.getElementById("ziti").innerHTML=localStorage.zitiname;}
-else
-{document.getElementById("content").style.fontFamily=localStorage.ziti;
-document.getElementById("ziti").innerHTML=localStorage.zitiname;}
-function ziti(){
-if(localStorage.ziti=="SimSun")
-{localStorage.ziti="SimHei";
-localStorage.zitiname="黑体"}
-else if(localStorage.ziti=="SimHei")
-{localStorage.zitiname="楷体";
-localStorage.ziti="KaiTi";}
-else
-{localStorage.zitiname="宋体";
-localStorage.ziti="SimSun";}
-document.getElementById("content").style.fontFamily=localStorage.ziti;
-document.getElementById("ziti").innerHTML=localStorage.zitiname;
+if (!localStorage.ziti) {
+    localStorage.ziti = "SimSun";
+    localStorage.zitiname = "SimSun";
+    document.getElementById("content").style.fontFamily = localStorage.ziti;
+    document.getElementById("ziti").innerHTML = localStorage.zitiname;
+} else {
+    document.getElementById("content").style.fontFamily = localStorage.ziti;
+    document.getElementById("ziti").innerHTML = localStorage.zitiname;
 }
-    if(document.body.scrollWidth < 650){
-      document.getElementById('ziti').style.display = 'none';
+
+function ziti() {
+    if (localStorage.ziti == "SimSun") {
+        localStorage.ziti = "SimHei";
+        localStorage.zitiname = "SimHei"
+    } else if (localStorage.ziti == "SimHei") {
+        localStorage.zitiname = "KaiTi";
+        localStorage.ziti = "KaiTi";
+    } else {
+        localStorage.zitiname = "SimSun";
+        localStorage.ziti = "SimSun";
     }
+    document.getElementById("content").style.fontFamily = localStorage.ziti;
+    document.getElementById("ziti").innerHTML = localStorage.zitiname;
+}
+if (document.body.scrollWidth < 650) {
+    document.getElementById('ziti').style.display = 'none';
+}
 
 //背景颜色
-if (!localStorage.bg)
-{localStorage.bg="#18222d";
-localStorage.content="#adadad";
-document.body.style.backgroundColor=localStorage.bg;
-document.getElementById("content").style.color=localStorage.content;}
-else
-{document.body.style.backgroundColor=localStorage.bg;
-document.getElementById("content").style.color= localStorage.content;}
-function bg(){
-if(localStorage.bg=="#ffffff")
-localStorage.bg="#f6f1e7";
-else if(localStorage.bg=="#f6f1e7")
-localStorage.bg="#dadada";
-else if(localStorage.bg=="#dadada")
-localStorage.bg="#dae9da";
-else if(localStorage.bg=="#dae9da")
-localStorage.bg="#ebcece";
-else if(localStorage.bg=="#ebcece")
-{localStorage.bg="#18222d";
-localStorage.content= "#adadad";
-document.getElementById("content").style.color= localStorage.content;}
-else if(localStorage.bg=="#18222d")
-{localStorage.bg="#ebddae";
-localStorage.content= "";
-document.getElementById("content").style.color=localStorage.content;}
-else if(localStorage.bg=="#ebddae")
-localStorage.bg="#dfecf0";
-else
-localStorage.bg="#ffffff";
-document.body.style.backgroundColor=localStorage.bg;
+if (!localStorage.bg) {
+    localStorage.bg = "#18222d";
+    localStorage.content = "#adadad";
+    document.body.style.backgroundColor = localStorage.bg;
+    document.getElementById("content").style.color = localStorage.content;
+} else {
+    document.body.style.backgroundColor = localStorage.bg;
+    document.getElementById("content").style.color = localStorage.content;
+}
+
+function bg() {
+    if (localStorage.bg == "#ffffff")
+        localStorage.bg = "#f6f1e7";
+    else if (localStorage.bg == "#f6f1e7")
+        localStorage.bg = "#dadada";
+    else if (localStorage.bg == "#dadada")
+        localStorage.bg = "#dae9da";
+    else if (localStorage.bg == "#dae9da")
+        localStorage.bg = "#ebcece";
+    else if (localStorage.bg == "#ebcece") {
+        localStorage.bg = "#18222d";
+        localStorage.content = "#adadad";
+        document.getElementById("content").style.color = localStorage.content;
+    } else if (localStorage.bg == "#18222d") {
+        localStorage.bg = "#ebddae";
+        localStorage.content = "";
+        document.getElementById("content").style.color = localStorage.content;
+    } else if (localStorage.bg == "#ebddae")
+        localStorage.bg = "#dfecf0";
+    else
+        localStorage.bg = "#ffffff";
+    document.body.style.backgroundColor = localStorage.bg;
 }
 
 
 
 let headers1 = new Headers();
-headers1.append('Authorization', 'Basic '+ btoa("dxh000130"+ ":" + "duan002349"));
+headers1.append('Authorization', 'Basic ' + btoa("dxh000130" + ":" + "duan002349"));
 headers1.append('Content-Type', 'application/json')
 headers1.append('Accept', 'text/plain')
 let articleid = 999;
@@ -105,7 +114,7 @@ const ArticleChoose = fetch("https://api.dxh000130.top/api/ArticleChoose", {
     })
 });
 ArticleChoose.then(res => {
-    res.json().then(function (return_text) {
+    res.json().then(function(return_text) {
         console.log(return_text.article)
         document.querySelector('#content').innerHTML = return_text.article
         error_remain = return_text.errorRemain;
@@ -121,25 +130,27 @@ textinput.addEventListener("keyup", function(event) {
         //console.log("回车");
         Enterbutton = 1;
         Login(Enterbutton)
-    }else{
+    } else {
         Enterbutton = 0;
         Login(Enterbutton, 0)
-        //console.log("没回车");
+            //console.log("没回车");
     }
 });
-function Hint(){
+
+function Hint() {
     document.getElementById("login-username").value = "";
     Login(0, 1);
 }
+
 function Login(Enterbutton, hint) {
     let headers2 = new Headers();
-    headers2.append('Authorization', 'Basic '+ btoa("dxh000130"+ ":" + "duan002349"));
+    headers2.append('Authorization', 'Basic ' + btoa("dxh000130" + ":" + "duan002349"));
     headers2.append('Content-Type', 'application/json')
     headers2.append('Accept', 'text/plain')
     var textinput = document.getElementById("login-username")
     var username = textinput.value;
 
-    if (username !== "" && username.search(" ")===-1){
+    if (username !== "" && username.search(" ") === -1) {
         //console.log(Enterbutton)
         const ArticleProcess = fetch("https://api.dxh000130.top/api/ArticleProcess", {
             method: "POST",
@@ -155,25 +166,25 @@ function Login(Enterbutton, hint) {
             })
         });
         ArticleProcess.then(res => {
-            res.json().then(function (return_text1) {
-                if (username !== ""){
+            res.json().then(function(return_text1) {
+                if (username !== "") {
                     //console.log(return_text1)
                     document.querySelector('#content').innerHTML = return_text1.articleDisp
-                    if (return_text1.correct === "yes,add 1 score"){
+                    if (return_text1.correct === "yes,add 1 score") {
                         wholearticle = return_text1.article;
                         AlreadyCorrect = return_text1.alreadyCorrect;
                         console.log(return_text1.correct + "Score:" + return_text1.score);
-                    }else if (return_text1.correct !== "No, No plus or minus score"){
+                    } else if (return_text1.correct !== "No, No plus or minus score") {
                         console.log(return_text1.correct + "Score:" + return_text1.score);
                     }
-                }else{
+                } else {
                     document.querySelector('#content').innerHTML = wholearticle;
                 }
             })
 
         })
-    }else {
-        if (hint === 1){
+    } else {
+        if (hint === 1) {
             const ArticleProcess = fetch("https://api.dxh000130.top/api/ArticleProcess", {
                 method: "POST",
                 credentials: 'include',
@@ -188,86 +199,82 @@ function Login(Enterbutton, hint) {
                 })
             });
             ArticleProcess.then(res => {
-                res.json().then(function (return_text1) {
+                res.json().then(function(return_text1) {
                     console.log(return_text1.hint);
                     document.querySelector('#content').innerHTML = return_text1.articleDisp;
-                })})
+                })
+            })
         }
         document.querySelector('#content').innerHTML = wholearticle;
     }
 }
 //屏宽
-if (!localStorage.pingkuan){
-     localStorage.pingkuan="700px";
-     document.getElementById("content").style.maxWidth=localStorage.pingkuan;
-    }
-    else {     document.getElementById("content").style.maxWidth=localStorage.pingkuan;}
+if (!localStorage.pingkuan) {
+    localStorage.pingkuan = "700px";
+    document.getElementById("content").style.maxWidth = localStorage.pingkuan;
+} else { document.getElementById("content").style.maxWidth = localStorage.pingkuan; }
 
-function pingkuan(){
-if(localStorage.pingkuan=="700px")
-	localStorage.pingkuan="800px";
-else if(localStorage.pingkuan=="800px")
-	localStorage.pingkuan="900px";
-else if(localStorage.pingkuan=="900px")
-		localStorage.pingkuan="1000px";
-else if(localStorage.pingkuan=="1000px")
-		localStorage.pingkuan="1100px";
-else if(localStorage.pingkuan=="1100px")
-		localStorage.pingkuan="1200px";
-	else if(localStorage.pingkuan=="1200px")
-		localStorage.pingkuan="1300px";
-else if(localStorage.pingkuan=="1300px")
-		localStorage.pingkuan="100%";
-else if(localStorage.pingkuan=="100%")
-		localStorage.pingkuan="600px";
-else if(localStorage.pingkuan=="600px")
-		localStorage.pingkuan="700px";
-   document.getElementById("content").style.maxWidth=localStorage.pingkuan;
+function pingkuan() {
+    if (localStorage.pingkuan == "700px")
+        localStorage.pingkuan = "800px";
+    else if (localStorage.pingkuan == "800px")
+        localStorage.pingkuan = "900px";
+    else if (localStorage.pingkuan == "900px")
+        localStorage.pingkuan = "1000px";
+    else if (localStorage.pingkuan == "1000px")
+        localStorage.pingkuan = "1100px";
+    else if (localStorage.pingkuan == "1100px")
+        localStorage.pingkuan = "1200px";
+    else if (localStorage.pingkuan == "1200px")
+        localStorage.pingkuan = "1300px";
+    else if (localStorage.pingkuan == "1300px")
+        localStorage.pingkuan = "100%";
+    else if (localStorage.pingkuan == "100%")
+        localStorage.pingkuan = "600px";
+    else if (localStorage.pingkuan == "600px")
+        localStorage.pingkuan = "700px";
+    document.getElementById("content").style.maxWidth = localStorage.pingkuan;
 }
-    if(document.body.scrollWidth < 600){
-      document.getElementById('pingkuan').style.display = 'none';
-    }
+if (document.body.scrollWidth < 600) {
+    document.getElementById('pingkuan').style.display = 'none';
+}
 
 //全屏
-     function ExecuteFun(a) {
-        var full = a.innerHTML == '全屏';
-        full ? fullscreen() : exitfullscreen();
-        a.innerHTML = full ? '缩屏' : '全屏';
-        return false;
-    }
+function ExecuteFun(a) {
+    var full = a.innerHTML == '全屏';
+    full ? fullscreen() : exitfullscreen();
+    a.innerHTML = full ? '缩屏' : '全屏';
+    return false;
+}
 
 function fullscreen() {
-  var el = document.documentElement,
-    rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen,
-    wscript;
-  if(typeof rfs != "undefined" && rfs) {
-    rfs.call(el);
-    return;
-  }
-  if(typeof window.ActiveXObject != "undefined") {
-    wscript = new ActiveXObject("WScript.Shell");
-    if(wscript) {
-      wscript.SendKeys("{F11}");
+    var el = document.documentElement,
+        rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen,
+        wscript;
+    if (typeof rfs != "undefined" && rfs) {
+        rfs.call(el);
+        return;
     }
-  }
+    if (typeof window.ActiveXObject != "undefined") {
+        wscript = new ActiveXObject("WScript.Shell");
+        if (wscript) {
+            wscript.SendKeys("{F11}");
+        }
+    }
 }
 
 function exitfullscreen() {
-  var el = document,
-    cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen,
-    wscript;
-  if (typeof cfs != "undefined" && cfs) {
-   cfs.call(el);
-   return;
-  }
-  if (typeof window.ActiveXObject != "undefined") {
-    wscript = new ActiveXObject("WScript.Shell");
-    if (wscript != null) {
-      wscript.SendKeys("{F11}");
+    var el = document,
+        cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen,
+        wscript;
+    if (typeof cfs != "undefined" && cfs) {
+        cfs.call(el);
+        return;
     }
- }
+    if (typeof window.ActiveXObject != "undefined") {
+        wscript = new ActiveXObject("WScript.Shell");
+        if (wscript != null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
 }
-
-
-
- 
